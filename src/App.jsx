20c5efc7412130fa6c576cb0c8e230bfc3b326d5
¨Camila <cami.2109@hotmail.com>
2024-06-22 08:37:12 -0300

    CommitCau

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ContextProvider, ContextGlobal } from './Components/utils/global.context';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Home from './Routes/Home';
 import Detail from './Routes/Detail';
 import Contact from './Routes/Contact';
 import Favs from './Routes/Favs';
-import './index.css'
-function App() {
-  return (
-      <div className="App">
-          <Navbar/>
-          <Routes>
-          <Route path="/" Component={Home}/>
-          <Route path="/detail/:id" Component={Detail}/>
-          <Route path="/contact" Component={Contact}/>
-          <Route path="/favs" Component={Favs}/>
-          </Routes>
+import './index.css';
 
-          <Footer/>
-      </div>
+const App = () => {
+  return (
+    <ContextProvider>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
+    </ContextProvider>
   );
-}
+};
+
+const AppContent = () => {
+  const { state } = useContext(ContextGlobal);
+  const { theme } = state;
+
+  return (
+    <div className={theme}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/favs" element={<Favs />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
+
